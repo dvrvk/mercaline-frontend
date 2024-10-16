0202e2d05f6bb179d2483458018e2ecd72b423ca
Diego Vázquez <99550587+dvrvk@users.noreply.github.com>
2024-10-16 19:03:33 +0200

    feat: control de errores de conexión autenticacion y estilo del login

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService, ProductResponseSummaryDTO, Page } from '../../services/product-service/product.service';
 import { CommonModule } from '@angular/common';
 import { CustomCurrencyFormatPipe } from '../../utils/custom-currency/custom-currency-format.pipe';
+import { UserServiceService } from '../../services/user-service/user-service.service';
+import { Router } from '@angular/router';
+
+declare var Swal: any;
 
 @Component({
   selector: 'app-product-list',
@@ -18,7 +22,10 @@ export class ProductListComponent {
   currentPage: number = 0;
   pageSize: number = 10;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService, 
+    private UserService: UserServiceService,
+    private router: Router) {}
 
   ngOnInit() : void {
     this.loadProducts(this.currentPage, this.pageSize);
@@ -27,14 +34,31 @@ export class ProductListComponent {
   loadProducts(page: number, size: number) : void {
     this.productService.getProduct(page, size).subscribe(
       (data: Page<ProductResponseSummaryDTO>) => {
-        console.log(data)
+        
         this.products = data.content;
         this.totalElements = data.page.totalElements;
         this.totalPages = data.page.totalPages;
         this.currentPage = data.page.number;
       }, 
       (error) => {
-        console.error('Error al cargar los productos', error)
+        console.error('Error al cargar los productos', error.error)
+
+        const message = error.error && error.error.message ? error.error.message : "Ha ocurrido un error al cargar los productos. Por favor, inténtalo de nuevo"
+
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Error al cargar los productos",
+          text: message,
+          showConfirmButton: true
+        });
+
+        // Si el token no es valido se hace logout()
+        if(Number(error.error.status) === 401) {
+          this.UserService.logOut();
+          this.router.navigate(["/login"]);
+          
+        }
       }
     );
   }
