4b4141c1848829584e14de06d97c360490dab153
Carlos Ramos <carlosramosmuga@gmail.com>
2024-12-08 19:47:50 +0100

    Añadida feature: Marcar producto como vendido

--- src/app/services/product-service/product.service.ts
+++ src/app/services/product-service/product.service.ts
@@ -4,61 +4,87 @@ import { catchError, Observable, throwError } from 'rxjs';
 import { SafeUrl } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ProductService {
-
   private apiUrlCategories = 'http://localhost:8080/products/categories';
   private apiUrlStatus = 'http://localhost:8080/products/status';
-  private apiUrlUser = 'http://localhost:8080/user/products'
+  private apiUrlUser = 'http://localhost:8080/user/products';
   private apiUrlfindByCategory = `http://localhost:8080/products/category/`;
   private apiUrlFilter = 'http://localhost:8080/products/filter2';
   private apiUrlUpload = 'http://localhost:8080/products/create';
-  private apiUrlImageMain = 'http://localhost:8080/images/main/'
-  private apiUrlImages = 'http://localhost:8080/images'
+  private apiUrlImageMain = 'http://localhost:8080/images/main/';
+  private apiUrlImages = 'http://localhost:8080/images';
   private apiUrlProductDetails = 'http://localhost:8080/products/';
   private apiUrlProductIsMine = 'http://localhost:8080/products/is-mine/';
-  private apiUrlUpdate = 'http://localhost:8080/products/update'
-  private apiUrlUserProducts = 'http://localhost:8080/products/myproducts'
-  private apiUrlDeleteProduct = 'http://localhost:8080/products/delete/'
+  private apiUrlUpdate = 'http://localhost:8080/products/update';
+  private apiUrlUserProducts = 'http://localhost:8080/products/myproducts';
+  private apiUrlDeleteProduct = 'http://localhost:8080/products/delete/';
+  private apiUrlMarkAsSold = 'http://localhost:8080/products/mark-as-sold/';
+  private apiUrlMarkAsAvailable = 'http://localhost:8080/products/mark-as-available/';
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getProduct(page: number, size: number): Observable<Page<ProductResponseSummaryDTO>> {
-
+  getProduct(
+    page: number,
+    size: number
+  ): Observable<Page<ProductResponseSummaryDTO>> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    const params = new HttpParams().set('page', page.toString()).set('size', size.toString()).set('sort', 'createDate');
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('sort', 'createDate');
 
-    return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlUser, { headers, params });
-
+    return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlUser, {
+      headers,
+      params,
+    });
   }
 
-  getUserProducts(page: number | null, size: number | null): Observable<Page<ProductResponseSummaryDTO>> {
+  getUserProducts(
+    page: number | null,
+    size: number | null
+  ): Observable<Page<ProductResponseSummaryDTO>> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     if (page != null && size != null) {
-      const params = new HttpParams().set('page', page.toString()).set('size', size.toString());
-      return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlUserProducts, { headers, params });
+      const params = new HttpParams()
+        .set('page', page.toString())
+        .set('size', size.toString());
+      return this.http.get<Page<ProductResponseSummaryDTO>>(
+        this.apiUrlUserProducts,
+        { headers, params }
+      );
     } else {
-      return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlCategories, { headers })
+      return this.http.get<Page<ProductResponseSummaryDTO>>(
+        this.apiUrlCategories,
+        { headers }
+      );
     }
   }
 
-  getCategories(page: number | null, size: number | null): Observable<Page<Categories>> {
+  getCategories(
+    page: number | null,
+    size: number | null
+  ): Observable<Page<Categories>> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     if (page != null && size != null) {
-      const params = new HttpParams().set('page', page.toString()).set('size', size.toString());
-      return this.http.get<Page<Categories>>(this.apiUrlCategories, { headers, params });
+      const params = new HttpParams()
+        .set('page', page.toString())
+        .set('size', size.toString());
+      return this.http.get<Page<Categories>>(this.apiUrlCategories, {
+        headers,
+        params,
+      });
     } else {
-      return this.http.get<Page<Categories>>(this.apiUrlCategories, { headers })
+      return this.http.get<Page<Categories>>(this.apiUrlCategories, {
+        headers,
+      });
     }
-
   }
 
   getStatus(): Observable<Status[]> {
@@ -71,28 +97,38 @@ export class ProductService {
   getProductsByCategory(page: number, size: number, categoryId: number) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    const params = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
 
-    return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlfindByCategory + categoryId, { headers, params });
+    return this.http.get<Page<ProductResponseSummaryDTO>>(
+      this.apiUrlfindByCategory + categoryId,
+      { headers, params }
+    );
   }
 
-  getProductsFilter(page: number, size: number, categoryId: number, filter: FilterClass | null, order: string | null): Observable<Page<ProductResponseSummaryDTO>> {
-
+  getProductsFilter(
+    page: number,
+    size: number,
+    categoryId: number,
+    filter: FilterClass | null,
+    order: string | null
+  ): Observable<Page<ProductResponseSummaryDTO>> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     let params = new HttpParams()
       .set('page', page.toString())
       .set('size', size.toString())
-      .set('categoryId', categoryId)
+      .set('categoryId', categoryId);
     if (filter !== null) {
-      params = params.set('status', filter.statusFilter.toString())
+      params = params.set('status', filter.statusFilter.toString());
 
       if (filter.minPrice !== null) {
-        params = params.set('minPrice', filter.minPrice)
+        params = params.set('minPrice', filter.minPrice);
       }
 
       if (filter.maxPrice !== null) {
-        params = params.set('maxPrice', filter.maxPrice.toString())
+        params = params.set('maxPrice', filter.maxPrice.toString());
       }
     }
 
@@ -100,8 +136,10 @@ export class ProductService {
       params = params.set('sort', order);
     }
 
-    return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlFilter, { headers, params });
-
+    return this.http.get<Page<ProductResponseSummaryDTO>>(this.apiUrlFilter, {
+      headers,
+      params,
+    });
   }
 
   uploadProduct(product: FormData) {
@@ -114,57 +152,78 @@ export class ProductService {
   getProductImage(id: number): Observable<Blob | URL> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.get(this.apiUrlImageMain + id, { headers, responseType: 'blob' })
+    return this.http.get(this.apiUrlImageMain + id, {
+      headers,
+      responseType: 'blob',
+    });
   }
 
-  getProductImages(productId: number) : Observable<ApiResponse> {
+  getProductImages(productId: number): Observable<ApiResponse> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<ApiResponse>(`${this.apiUrlImages}/${productId}`, {headers});
+    return this.http.get<ApiResponse>(`${this.apiUrlImages}/${productId}`, {
+      headers,
+    });
   }
 
-  getProductDetails(productId : number) {
+  getProductDetails(productId: number) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<any>(`${this.apiUrlProductDetails}${productId}`, {headers})
+    return this.http.get<any>(`${this.apiUrlProductDetails}${productId}`, {
+      headers,
+    });
   }
 
-  checkIsMine(productId : number) {
+  checkIsMine(productId: number) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<any>(`${this.apiUrlProductIsMine}${productId}`, {headers})
+    return this.http.get<any>(`${this.apiUrlProductIsMine}${productId}`, {
+      headers,
+    });
   }
 
-  putProduct(form : FormData) {
+  putProduct(form: FormData) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.put(this.apiUrlUpdate,form,{headers});
+    return this.http.put(this.apiUrlUpdate, form, { headers });
   }
 
-  deleteProduct(id : number) {
+  deleteProduct(id: number) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.delete(`${this.apiUrlDeleteProduct}${id}`,{headers});
-
+    return this.http.delete(`${this.apiUrlDeleteProduct}${id}`, { headers });
   }
 
+  markProductAsSold(productId: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(`${this.apiUrlMarkAsSold}${productId}`,{},{ headers }
+    );
+  }
+
+  markProductAsAvailable(productId: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(`${this.apiUrlMarkAsAvailable}${productId}`, {}, { headers });
+  }
+  
 }
 
-export interface ApiResponse { 
-  status: string; 
-  fecha: string; 
-  mensaje: string; 
+export interface ApiResponse {
+  status: string;
+  fecha: string;
+  mensaje: string;
 }
 
 export interface FilterClass {
-  statusFilter: number[],
-  maxPrice: number | null,
-  minPrice: number | null
+  statusFilter: number[];
+  maxPrice: number | null;
+  minPrice: number | null;
 }
 
 export interface Categories {
@@ -178,15 +237,15 @@ export interface Status {
 }
 
 export interface ProductResponseSummaryDTO {
-  id: number,
-  name: string,
-  description: string,
-  status: string,
-  imageUrl: SafeUrl | string,
-  price: number,
-  category: string,
-  createDate : Date,
-  seller: ResponseUserProductDTO
+  id: number;
+  name: string;
+  description: string;
+  status: string;
+  imageUrl: SafeUrl | string;
+  price: number;
+  category: string;
+  createDate: Date;
+  seller: ResponseUserProductDTO;
 }
 
 export interface ResponseUserProductDTO {
@@ -206,8 +265,7 @@ export interface ProductRequestDTO {
 
 export interface Page<T> {
   content: T[];
-  page: PageContent
-
+  page: PageContent;
 }
 
 export interface PageContent {
@@ -216,6 +274,3 @@ export interface PageContent {
   size: number;
   number: number;
 }
-
-
-
